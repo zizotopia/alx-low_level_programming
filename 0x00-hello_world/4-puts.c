227a0a1da08feacd1b3e4700f18b3f3e78c048e8
zizotopia <zizoknightboy07@yahoo.com>
2023-06-15 19:24:11 -0700

    O

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#imclude <stdio>
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);    
+#include <stdio.h>
+/**
+ * main - This is the entry point of the project
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
