53d3459beda23567ba0989932c985c919bb4ec90
zizotopia <zizoknightboy07@yahoo.com>
2023-06-16 17:12:41 -0700

    TMAM

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Enty point
+ *
+ * Description: print add alphabet letters
+ *
+ * Return: A;ways 0 (Success
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	
+	while (ch <= 'z');
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}	
