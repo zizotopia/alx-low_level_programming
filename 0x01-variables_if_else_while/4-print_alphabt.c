9f9bbbfac0a2a6023ea54f2774cbe4326acd39c0
zizotopia <zizoknightboy07@yahoo.com>
2023-06-16 18:00:18 -0700

    lol

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print add alphabet letters exept q and e
+ *
+ * Retutrn: Always 0 (Success)
+ * */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (xh == 'e' || ch == 'q')
+			chh++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
