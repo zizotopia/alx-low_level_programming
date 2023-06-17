64b6e5268d3709723a1b2f35358d52b4c3ca9db0
zizotopia <zizoknightboy07@yahoo.com>
2023-06-16 17:39:38 -0700

    ZOZ

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print alphabet on lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/*prints a - z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		puchar(CH);
+		CH++
+	}
+	puchar('\n');
+
+	return (0);
+}
