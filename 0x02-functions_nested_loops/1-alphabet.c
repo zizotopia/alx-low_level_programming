14999d9bd33e93d8bb5fe6a9a9564bd402a2d86a
zizotopia <zizoknightboy07@yahoo.com>
2023-06-20 17:31:31 -0700

    HAA

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * print_alphabet - utilizes on the _putchar function to print the alphanet a - z
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+}
