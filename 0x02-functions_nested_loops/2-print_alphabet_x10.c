e2ad1ce51efc39fdae50555d1d93d43c77bf71cb
zizotopia <zizoknightboy07@yahoo.com>
2023-06-20 17:48:36 -0700

    lol

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphanet 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9l line++)
+	{
+		for (line = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_puchar('\n');
+	}
+}
