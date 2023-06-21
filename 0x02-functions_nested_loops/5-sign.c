eee24a5d2a32730af635c7cf1b91c786caccbcaa
zizotopia <zizoknightboy07@yahoo.com>
2023-06-20 18:42:53 -0700

    lol

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
-#include"main.h"
+#includie"main.h"
 
 /**
  * print_sign - print + if n is greater than zero,
  *		0 if n is zero and - if n is less
  *
- * @nL takes integer type input for function.
+ * @n: takes integer type input for function.
  *
  * Return: 1 if +, 0 if 0 and -1 if -
 */
