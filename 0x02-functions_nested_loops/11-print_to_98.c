726bc623210ca4321b3bd23376d1bb0f13388a4e
zizotopia <zizoknightboy07@yahoo.com>
2023-06-20 19:50:05 -0700

    lol

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,25 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_to_98 - print n to 98 counts
- * 		separtated by comma, followed
- * 		by space and numbers should be
- * 		printed in order
- * @n: input
+*print_to_98 - prints natural
+*numbers from n to 98
+*@n:integer to start by
+*return: returns nothing
 */
-
 void print_to_98(int n)
 {
-	int count;
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
 
-	if (n > 98)
-		for (count = n; count > 98; count--)
-			printf("%d, ", count);
-	else
-		for (count = n; count < 98; count++)
-			printf("%d, ", count);
-	printf("98\n");
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+
+printf("%d\n", n);
+}
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,28 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * times_table - prints the 9 times table
- *
- * Example Tanle
- * 0, 0, 0, ..
- * 0, 1, 2, 3, ..
+*times_table - prints the 9 times table
+*
+*Return: returns nothing
 */
-
 void times_table(void)
 {
-	int num, mult, prod;
+int digit, mult, result;
+for (digit = 0; digit <= 9; digit++)
+{
+_putchar('0');
 
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar(48);
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar('.');
-			_putchar(' ');
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 9)
+_putchar(' ');
+else
+_putchar((result / 10) + '0');
 
-			prod = num * mult;
-			/*
-			 * put space if product is a single number
-			 * place the first digit if its two numbers
-			*/
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + 48); /*get the first digit*/
-			_putchar((prod % 10) + 48); /*get the second digit*/
-		}
-		_putchar('\n');
-	}
+_putchar((result % 10) + '0');
 }
+_putchar('\n');
+}
+
