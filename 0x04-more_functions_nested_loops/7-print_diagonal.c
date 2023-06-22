2574e02019a1ce141b69ec76d210d07b2f69f5f2
zizotopia <zizoknightboy07@yahoo.com>
2023-06-22 16:36:07 -0700

    LOL

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,7 +17,7 @@ void print_diagonal(int n)
 	{
 		for (postn = 1; postn <= n; postn++)
 		{
-			for (space = 1; space <= postn++)
+			for (space = 1; space <= postn; postn++)
 				_putchar(' ');
 		_putchar(92); /*is equal to '/' char*/
 		_putchar('\n');
