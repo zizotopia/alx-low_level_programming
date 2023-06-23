b83a11f6f2a67dfc8e05a0bf157e2230d8f471da
zizotopia <zizoknightboy07@yahoo.com>
2023-06-22 17:39:20 -0700

    LOL

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - prints an integer.
+ * print_number - prints an integer.
  *
  * @n: The integer to be printed.
 */
-void print_numbers(int n)
+void print_number(int n)
 {
 	unsigned int num = n;
 
