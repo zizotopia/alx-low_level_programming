89a2c388f0ad44f95cc88dc2de74eecf490bf16a
zizotopia <zizoknightboy07@yahoo.com>
2023-06-22 18:00:05 -0700

    LOL

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,21 +7,19 @@
  * Return: void
  */
 
-
 void print_diagonal(int n)
 {
-        int i, j;
+	int i, j;
 
-        if (n <= 0)
-                _putchar('\n');
-        for (i = 0; i < n; i++)
-        {
-                for (j = 0; j < i; j++)
-                {
-                        _putchar(' ');
-                }
-                _putchar('\\');
-                _putchar('\n');
-        }
-
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
