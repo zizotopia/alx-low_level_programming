d4eddc2e1e871556b05cb18070679d29f4f02925
zizotopia <zizoknightboy07@yahoo.com>
2023-06-26 15:42:51 -0700

    lol

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main(void)
+{
+	int n;
+	int *b;
+
+	n = 402;
+	printf("n = %d \n", n);
+	
+	*b = 98;
+	printf("n = %d", *b);
+    
+	return 0;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+El ZoZ El Rayek Mosabeb El Harayek
