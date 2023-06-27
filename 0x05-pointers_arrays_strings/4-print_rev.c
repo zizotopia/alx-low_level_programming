440423bda2cc0cf71ee2e7a4b2d518964d25aa4b
zizotopia <zizoknightboy07@yahoo.com>
2023-06-26 19:32:35 -0700

    lol

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,15 @@
 
 void print_rev(char *s)
 {
-	int i, n;
+        int i, n;
 
-	n = 0;
-	while (s[n] != '\0')
-		n++;
+        n = 0;
+        while (s[n] != '\0')
+                n++;
 
-	for (i = n - 1; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+        for (i = n - 1; i >= 0; i--)
+        {
+                _putchar(s[i]);
+        }
+        _putchar('\n');
 }
