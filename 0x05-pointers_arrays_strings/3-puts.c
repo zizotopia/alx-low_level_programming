fdcfd11008f35ee3ec99de24314db83e8cf90429
zizotopia <zizoknightboy07@yahoo.com>
2023-06-26 19:37:57 -0700

    lol

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _puts - prints a string
- * @str: the string
+ *_puts - prints a string
+ * @str: string to print
  *
- * Return: the length of the string
+ * Description: prints a string
+ * On success: returns no error
  */
+
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
+		putchar(*(str + i));
 		i++;
 	}
-	_putchar('\n');
+	putchar(10);
 }
