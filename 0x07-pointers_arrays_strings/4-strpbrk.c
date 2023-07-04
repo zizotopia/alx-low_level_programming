9520a65e0840b8e5c8b4344d51f500040c52b058
zizotopia <zizoknightboy07@yahoo.com>
2023-07-04 16:21:41 -0700

    leeh

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,43 +1,25 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _strpbrk - searches a string for any set of bytes
- * @s: string to be searched
- * @accept: string to be used
+ * _strpbrk - function that searches a string for any of a set of bytes
  *
- * Return: a pointer to the byte in s that matches one of the bytes in accept
+ * @s:first occurrence in the string
+ *
+ * @accept: matches one of the bytes, or @NULL if no such byte
+ *
+ * Return: a pointer to the byte
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-
-	for (; s[i] != '\0' && _strchr(accept, s[i]) == NULL; i++)
-		;
-
-	if (s[i] == '\0')
-		return (NULL);
-	else
-		return (s + i);
-}
-
-/**
- * _strchr - locates a char in a string
- * @s: string to be searched
- * @c: char to be checked
- *
- * Return: pointer to the first occurence of c in s
- */
-
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	for (; s[i] != c && s[i] != '\0'; i++)
-		;
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+	int i;
+		while (*s)
+		{
+			for (i = 0; accept[i]; i++)
+			{
+				if (*s == accept[i])
+					return (s);
+			}
+			s++;
+		}
+	return (NULL);
 }
