323f03681cd97b3c5879fd13835760de378944bb
zizotopia <zizoknightboy07@yahoo.com>
2023-07-07 07:56:42 -0700

    static lib

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if itsuppercase, 0 if not
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
