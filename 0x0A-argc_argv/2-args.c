b4f3894fa25ad21c37d79572411500d312c8c1f7
zizotopia <zizoknightboy07@yahoo.com>
2023-07-07 16:22:43 -0700

    Hi

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed int the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
