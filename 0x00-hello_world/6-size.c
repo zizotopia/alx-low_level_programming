3ab798bbe1dd01ee17c92a0c7b7a72b27d32ce49
zizotopia <zizoknightboy07@yahoo.com>
2023-07-08 17:03:16 -0700

    Hi

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,12 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
 
-		printf("Size of a char: %d", sizeof(a));
-		printf("Size of an int: %d", sizeof(b));
-		printf("Size of a long int: %d", sizeof(c));
-		printf("Size of a long long int: %d", sizeof(d));
-		printf("Size of a float: %d", sizeof(e));
+		printf("Size of a char: %d", sizeof(char));
+		printf("Size of an int: %d", sizeof(int));
+		printf("Size of a long int: %d", sizeof(long int));
+		printf("Size of a long long int: %d", sizeof(long long int));
+		printf("Size of a float: %d", sizeof(float));
 
 		return (0);
 }
