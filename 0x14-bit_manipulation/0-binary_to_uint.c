d6a376a75d14ab15a19f5617fb6169909d2ad58e
zizotopia <zizoknightboy07@yahoo.com>
2023-10-26 18:08:05 -0700

    Hi

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,24 +2,47 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary numbers string to unsigned int
- * @b: the binary number as a string
+ * check_valid_string - checks if a string has only 0's and 1's
+ * @b: string to be checked
  *
- * Return: the unsigned int from of b
-*/
-
-unsigned int binary_to_uint(const char *b)
+ * Return: 1 if string is valid, 0 otherwise
+ */
+int check_valid_string(const char *b)
 {
-	unsigned int num = 0;
-
-	if (!b)
+	if (b == NULL)
 		return (0);
 	while (*b)
 	{
-		if (*b != '0' && *b != '1')
+		if (*b != '1' && *b != '0')
 			return (0);
-		num = num * 2 + (*b++ - '0';
+		b++;
 	}
 
-	return (num);
+	return (1);
+}
+
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the converted value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int str_len = 0, base = 1;
+
+	if (!check_valid_string(b))
+		return (0);
+	while (b[str_len] != '\0')
+		str_len++;
+	while (str_len)
+	{
+		decimal += ((b[str_len - 1] - '0') * base);
+		base *= 2;
+		str_len--;
+	}
+
+	return (decimal);
 }
