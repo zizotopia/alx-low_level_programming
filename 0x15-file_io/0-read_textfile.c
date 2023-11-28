159caccde05f207dfb16b9cdd626afdf8aace4d6
zizotopia <zizoknightboy07@yahoo.com>
2023-11-28 13:27:36 -0800

    Hello

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,39 @@
-i#include "main.h"
-#include <stddef.h>
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file
- * @letters:  number of letters to be printed
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: number of letters read and printed
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, n_read, wrote;
+	ssize_t o, r, w;
 	char *buffer;
 
-	buffer = malloc(sizeof(*buffer) * (letters + 1));
-	if (filename == NULL || buffer == NULL)
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-		return (0);
-	n_read = read(file, buffer, letters);
-	if (n_read == -1)
-		return (0);
-	buffer[n_read] = '\0';
-	wrote = write(STDOUT_FILENO, buffer, n_read);
-	if (wrote != n_read)
-		return (0);
+
 	free(buffer);
-	close(file);
-	return (n_read);
+	close(o);
+
+	return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
+/*
+ * File: holberton.h
+ * Auth: Godswill Kalu
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
+
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
