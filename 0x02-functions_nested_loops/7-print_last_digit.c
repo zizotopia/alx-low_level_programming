ada6bf15249fb76e5fc1c1a794692cefa7583fc3
zizotopia <zizoknightboy07@yahoo.com>
2024-11-06 17:59:57 -0800

    Last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,12 @@
 
 int print_last_digit(int n)
 {
-	int lastDigit;
-
 	if (n < 0)
-		lastDigit = -1 * (n % 10);
+	{
+		_putchar('0' - n % 10);
+	}
 	else
-		lastDigit = n % 10;
-
-	_putchar(lastDigit + '0');
-	return (lastDigit);
+	{
+		_putchar('0' + n % 10);
+	}
 }
