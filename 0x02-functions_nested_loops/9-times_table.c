63f956da06d6778b804ad7dacc879356daa73cb9
zizotopia <zizoknightboy07@yahoo.com>
2024-11-09 15:15:34 -0800

    Tabel

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,23 +6,25 @@
 */
 void times_table(void)
 {
-int digit, mult, result;
-for (digit = 0; digit <= 9; digit++)
-{
-_putchar('0');
+	int z, d, x;
 
-for (mult = 1; mult <= 9; mult++)
-{
-_putchar(',');
-_putchar(' ');
-result = digit * mult;
-if (result <= 9)
-_putchar(' ');
-else
-_putchar((result / 10) + '0');
-
-_putchar((result % 10) + '0');
-}
-_putchar('\n');
-}
+	for (z = 0; z <= 9; z++)
+	{
+		for (d = 0; d <= 9; d++)
+		{
+			x = z * d;
+			if (x <= 9)
+			{
+				putchar(' ');
+			}
+			printf("%d", x);
+			if (d <= 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('$');
+		putchar(10);
+	}
 }
